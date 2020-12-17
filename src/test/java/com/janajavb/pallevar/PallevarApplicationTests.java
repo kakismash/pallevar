571d4b9a07db29e721ed27ecd6849a0b7db1de96
Joaquin Navarro <alfian1991@gmail.com>
2020-12-16 23:01:22 -0500

    first commit

---
+++ src/test/java/com/janajavb/pallevar/PallevarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janajavb.pallevar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PallevarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
